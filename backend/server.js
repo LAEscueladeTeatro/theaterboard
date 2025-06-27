3e31333694781b4c5fc292620a7ac78e1497ce4f
LuisAc28 <luisacunach@gmail.com>
2025-06-27 14:21:22 -0500

    feat: Connection with backend 2.0.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./config/db');
+const pool = require('./db');
 
 // Importar Rutas
 const authRoutes = require('./routes/authRoutes');
